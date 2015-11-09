39107aef9c53ac3c90472fa23cfd42fb84deb9e1
Brock Hostetter <bstetterslc@gmail.com>
2015-11-09 09:55:57 -0700

    updated the layout of the GUI application

--- src/gui/view/GUIPanel.java
+++ src/gui/view/GUIPanel.java
@@ -13,11 +13,13 @@ public class GUIPanel extends JPanel
 	private GUIController baseController;
 	private JButton firstButton;
 	private JTextField firstField;
+	private SpringLayout baseLayot;
 	
 	public GUIPanel(GUIController baseController)
 	{
 		this.baseController = baseController;
 		
+		baseLayout = new SpringLayout();
 		firstButton = new JButton("Please do not click the button");
 		firstField = new JTextField("words can be typed here");
 		
@@ -25,13 +27,22 @@ public class GUIPanel extends JPanel
 		setupLayout();
 		setupListeners();
 	}
+	
+	/**
+	 * Helper method to load all GUI components into the panel
+	 */
 	private void setupPanel()
 	{
-		
+		this.setLayout(baseLayout);
+		this.add(firstButton);
+		this.add(firstField);
 	}
 	private void setupLayout()
 	{
-		
+		baseLayout.putConstrant(SpringLayout.WEST, firstButton, 107, SpringLayout.WEST, this);
+		baseLayout.putConstrant(SpringLayout.SOUTH, firstButton, -32, SpringLayout.SOUTH, this);
+		baseLayout.putConstrant(SpringLayout.WEST, firstField, 37, SpringLayout.WEST, this);
+		baseLayout.putConstrant(SpringLayout.WEST, firstField, -24, SpringLayout.SOUTH, this);
 	}
 	private void setupListeners()
 	{
