10400b047d8ffcc646d5b136e75f57aef28fee35
Brock Hostetter <bstetterslc@gmail.com>
2015-11-11 09:42:44 -0700

    updated the GUI panel

--- src/gui/view/GUIPanel.java
+++ src/gui/view/GUIPanel.java
@@ -3,6 +3,9 @@ package gui.view;
 import javax.swing.*;
 
 import gui.controller.GUIController;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
 /**
  * 
  * @author bhos1889
@@ -13,7 +16,7 @@ public class GUIPanel extends JPanel
 	private GUIController baseController;
 	private JButton firstButton;
 	private JTextField firstField;
-	private SpringLayout baseLayot;
+	private SpringLayout baseLayout;
 	
 	public GUIPanel(GUIController baseController)
 	{
@@ -37,16 +40,27 @@ public class GUIPanel extends JPanel
 		this.add(firstButton);
 		this.add(firstField);
 	}
+	
+	/**
+	 * Helper method for arranging the panel.
+	 * Dumping ground for generated code.
+	 */
 	private void setupLayout()
 	{
-		baseLayout.putConstrant(SpringLayout.WEST, firstButton, 107, SpringLayout.WEST, this);
-		baseLayout.putConstrant(SpringLayout.SOUTH, firstButton, -32, SpringLayout.SOUTH, this);
-		baseLayout.putConstrant(SpringLayout.WEST, firstField, 37, SpringLayout.WEST, this);
-		baseLayout.putConstrant(SpringLayout.WEST, firstField, -24, SpringLayout.SOUTH, this);
+		baseLayout.putConstraint(SpringLayout.WEST, firstButton, 107, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.SOUTH, firstButton, -32, SpringLayout.SOUTH, this);
+		baseLayout.putConstraint(SpringLayout.WEST, firstField, 37, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.WEST, firstField, -24, SpringLayout.SOUTH, this);
 	}
 	private void setupListeners()
 	{
-		
+		firstButton.addActionListener(new ActionListener()
+		{
+			public void actionPreformed(ActionEvent click)
+			{
+				firstField.setText("Wow, this is the most amazing click ever!");
+			}
+		});
 	}
 	
 }
